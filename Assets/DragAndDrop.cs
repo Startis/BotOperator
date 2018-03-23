1ccba864a8abcf29f025a7c56e6317b06b0c5dad
Unknown <s.debank@rubika-edu.com>
2018-03-23 18:27:47 +0100

    Last fix work

---
+++ Assets/DragAndDrop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public static GameObject draggedItem;
+
+	public void OnBeginDrag(PointerEventData eventData){
+		draggedItem = gameObject;
+		draggedItem.transform.SetAsLastSibling();
+	}
+
+	public void OnDrag(PointerEventData eventData){
+		if (draggedItem != null)
+			draggedItem.transform.position += (Vector3)eventData.delta;	
+	}
+
+	public void OnEndDrag(PointerEventData eventData){
+		draggedItem = null;
+	}
+}
