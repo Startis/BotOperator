cc45232e845f9e296309118154000677a4d0453d
Startis <startis@live.fr>
2018-04-02 02:54:08 +0200

    Work prez

--- Assets/Script/presmanager.cs
+++ Assets/Script/presmanager.cs
@@ -37,23 +37,22 @@ public class presmanager : MonoBehaviour {
 
 	void UpdateSlide (){
 		if (currentSlide < nombreSlide){
-			switch (currentSlide)
-			{
+			switch (currentSlide) {
 			case 0:
-				parentslides.transform.DOLocalMoveX(780, returnbegginingtime, false);
-				Debug.Log("blabla");
+				parentslides.transform.DOLocalMoveX (780, returnbegginingtime, false);
+				Debug.Log ("blabla");
 				break;
 			case 1:
 				parentslides.transform.DOLocalMoveX (-815, movetime, false);
-				Debug.Log("blabla1");
+				Debug.Log ("blabla1");
 				break;
 			case 2:
 				parentslides.transform.DOLocalMoveX (-2415, movetime, false);
-				Debug.Log("blabla2");
+				Debug.Log ("blabla2");
 				break;
 			case 3:
 				parentslides.transform.DOLocalMoveX (-4015, movetime, false);
-				Debug.Log("blabla3");
+				Debug.Log ("blabla3");
 				break;
 			case 4:
 				parentslides.transform.DOLocalMoveX (-5615, movetime, false);
@@ -61,7 +60,34 @@ public class presmanager : MonoBehaviour {
 				break;
 			case 5:
 				parentslides.transform.DOLocalMoveX (-7215, movetime, false);
-				Debug.Log("blabla5");
+				Debug.Log ("blabla5");
+				break;
+			case 6:
+				parentslides.transform.DOLocalMoveX (-8815, movetime, false);
+				Debug.Log ("blabla5");
+				break;
+			case 7:
+				parentslides.transform.DOLocalMoveX (-10415, movetime, false);
+				break;
+			case 8:
+				parentslides.transform.DOLocalMoveX (-12015, movetime, false);
+				Debug.Log ("blabla7");
+				break;
+			case 9:
+				parentslides.transform.DOLocalMoveX (-13615, movetime, false);
+				Debug.Log ("blabla8");
+				break;
+			case 10:
+				parentslides.transform.DOLocalMoveX (-15215, movetime, false);
+				Debug.Log ("blabla9");
+				break;
+			case 11:
+				parentslides.transform.DOLocalMoveX (-16815, movetime, false);
+				Debug.Log ("blabla10");
+				break;
+			case 12:
+				parentslides.transform.DOLocalMoveX (-18415, movetime, false);
+				Debug.Log ("blabla11");
 				break;
 			}
 		}else{
